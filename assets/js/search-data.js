211d3e7ca8583b86f3410130502122167f2e9df4
krorange <78900024+krorange@users.noreply.github.com>
2025-07-07 19:49:51 +0000

    Deploying to gh-pages from @ krorange/krorange.github.io@308a0f758360a3a765afed74caaa08a9449a8ebd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Materials for courses I&#39;ve taught at IU.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
